57503fc29a7057daa809682069081a289e9a5515
Esuabom Dijemeni <e.dijemeni@gmail.com>
2020-03-23 15:58:07 +0000

    Completed tutorial on console input on different data types

--- Tutorial01/main.cpp
+++ Tutorial01/main.cpp
@@ -11,6 +11,7 @@ int main(int argc, char **argv)
 **/
 
 // CPP 04 - Complile Output
+/**
 #include <iostream>
 
 using namespace std; 
@@ -25,5 +26,35 @@ int main(){
     cout << "Hello World " << endl << letter << endl << number << endl << name << endl << decimal << endl;
     return 0;
 }
+**/
+
+// CPP 05 - Compiler Input
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    string name = "Zach";
+    int age = 23;
+    cout << name << " is " << age <<  " years old." << endl << endl;
+    
+    // Get name 
+    string name_2;
+    cout << "Enter name 2: ";
+    cin >> name_2;
+    cout << endl << "You entered: " << name_2 << endl;
+    
+    // Get age 
+    int age_2;
+    cout << "Enter age 2: ";
+    cin >> age_2;
+    cout << endl << "You entered: " << age_2 << endl;
+    
+    
+    return 0;
+}
+
 
  
